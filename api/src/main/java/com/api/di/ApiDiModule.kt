67832c43c6f0f2c96183d9fcd940a3feab844811
Marcus Dias <marcus-dias@hotmail.com>
2020-12-05 16:26:05 +0000

    api module with di

---
+++ api/src/main/java/com/api/di/ApiDiModule.kt
@@ -0,0 +1,37 @@
+package com.api.di
+
+import com.api.ApiServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ApiDiModule {
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiServices =
+        retrofit.create(ApiServices::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("")
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+}
