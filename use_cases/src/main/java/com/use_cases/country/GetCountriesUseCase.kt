6a5f69f8829c8b95d31d5496a2ea908896c7e85f
Marcus Dias <marcus-dias@hotmail.com>
2020-12-06 00:17:32 +0000

    add coroutines to use cases

--- use_cases/src/main/java/com/use_cases/country/GetCountriesUseCase.kt
+++ use_cases/src/main/java/com/use_cases/country/GetCountriesUseCase.kt
@@ -1,9 +1,17 @@
 package com.use_cases.country
 
+import com.domain_model.Country
 import com.repositories_interfaces.country.CountryRepository
+import com.use_cases.DomainResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetCountriesUseCase @Inject constructor(
     private val countryRepository: CountryRepository
 ) {
+    suspend operator fun invoke(): DomainResult<List<Country>> =
+        withContext(Dispatchers.IO) {
+            DomainResult.Success(countryRepository.getCountries())
+        }
 }
